c745c441a7d4bc79d4861a8df27ce933c4bc4504
ats1999 <rkt82224@gmail.com>
2021-11-20 22:29:40 +0530

    tags doen

---
+++ pages/api/tag.js
@@ -0,0 +1,21 @@
+import User from "modals/User";
+import DB from "lib/db";
+import { getSession } from "next-auth/react";
+
+export default async function tag(req, res) {
+  await DB();
+  const session = await getSession({ req });
+  const userId = session.user._id;
+  if (req.method === "POST") {
+    await User.findOneAndUpdate(
+      { _id: userId },
+      { $push: { tags: req.body.tag } }
+    );
+    res.send("OK");
+  } else {
+    const tags = await User.findOne({ _id: userId }, { tags: 1 }).then(
+      (user) => user.tags
+    );
+    res.send(tags);
+  }
+}
--- pages/tags.js
+++ pages/tags.js
@@ -1,25 +1,56 @@
+import { useState, useEffect } from "react";
 import { VStack, Button, Heading } from "@chakra-ui/react";
 import CreatableSelect from "react-select/creatable";
+import axios from "axios";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
 export default function Tags() {
+  const [tags, setTags] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedTag, setSelectedTag] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api/tag")
+      .then((res) => setTags(res.data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const createTag = (tag) => {
+    setLoading(true);
+    axios
+      .post("/api/tag", { tag })
+      .then((res) => {
+        setTags([...tags, tag]);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
   return (
-    <VStack alignItems="left" p={2} mt={3} width="400px" maxWidth="100%" mx="auto">
+    <VStack
+      alignItems="left"
+      p={2}
+      mt={3}
+      width="400px"
+      maxWidth="100%"
+      mx="auto"
+    >
       <Heading as="h1" size="sm">
         {" "}
         Select a TAG to get started
       </Heading>
       <CreatableSelect
-        isClearable
-        onChange={(val) => console.log(val)}
-        onCreateOption={(val) => console.log(val)}
-        options={options}
+        onChange={setSelectedTag}
+        onCreateOption={createTag}
+        options={tags.map((tag) => {
+          return {
+            value: tag,
+            label: tag,
+          };
+        })}
+        isLoading={loading}
       />
-      <Button colorScheme="green" variant="solid">
+      <Button colorScheme="green" variant="solid"disabled={!selectedTag}>
         GO
       </Button>
     </VStack>
