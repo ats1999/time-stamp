1bb433fec22a7d892b92ede166cb0673bee225b9
ats1999 <rkt82224@gmail.com>
2021-11-21 10:38:56 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withPWA = require("next-pwa");
-
+const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    runtimeCaching,
     // disable: process.env.NODE_ENV === "development",
     sw: '/sw.js'
   },
