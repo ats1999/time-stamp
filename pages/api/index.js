c04290a9d3ab817a198282591f483e231c8e8094
ats1999 <rkt82224@gmail.com>
2021-11-21 15:20:10 +0530

    logaa

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,12 +1,6 @@
-export default function handler(req, res) {
-  const clientId = process.env.GOOGLE_CLIENT_ID;
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-  const uri = process.env.MONGO_URI;
-  const url = process.env.NEXTAUTH_URL;
-  res.status(200).send({
-    GOOGLE_CLIENT_ID: "clientId",
-    GOOGLE_CLIENT_SECRET: "clientSecret",
-    MONGO_URI: "uri",
-    NEXTAUTH_URL: url,
-  });
+import { getSession } from "next-auth/react";
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  res.status(200).send(session);
 }
