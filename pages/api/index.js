a0af335c62d3fd418b521da3bd91ef0516cfcdf8
ats1999 <rkt82224@gmail.com>
2021-11-21 15:26:38 +0530

    as

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,5 +2,5 @@ import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
-  res.status(200).send(session);
+  res.status(200).send({session});
 }
