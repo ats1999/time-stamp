1223fee3b63e70ab8fd5e3433214a020f7131c31
ats1999 <rkt82224@gmail.com>
2021-11-21 08:55:01 +0530

    tmp

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -100,28 +100,7 @@ export default function Hero() {
             width={"full"}
             overflow={"hidden"}
           >
-            <IconButton
-              aria-label={"Play Button"}
-              variant={"ghost"}
-              _hover={{ bg: "transparent" }}
-              icon={<PlayIcon w={12} h={12} />}
-              size={"lg"}
-              color={"white"}
-              position={"absolute"}
-              left={"50%"}
-              top={"50%"}
-              transform={"translateX(-50%) translateY(-50%)"}
-            />
-            <Image
-              alt={"Hero Image"}
-              fit={"cover"}
-              align={"center"}
-              w={"100%"}
-              h={"100%"}
-              src={
-                "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
-              }
-            />
+            <iframe width="100%" height="100%" src="https://embed.lottiefiles.com/animation/67011"></iframe>
           </Box>
         </Flex>
       </Stack>
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
-  res.status(200).send("Welcome!");
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  const uri = process.env.MONGO_URI;
+  res.status(200).send({
+    clientId,
+    clientSecret,
+    uri,
+  });
 }
