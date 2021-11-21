9f8cbc8b9d0a2ef433e980ab2d5373b016eb6e90
ats1999 <rkt82224@gmail.com>
2021-11-21 09:07:01 +0530

    added url

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,9 +2,11 @@ export default function handler(req, res) {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
   const uri = process.env.MONGO_URI;
+  const url  = process.env.NEXTAUTH_URL
   res.status(200).send({
     clientId,
     clientSecret,
     uri,
+    url
   });
 }
