94c28f2b79719fb9f362410771d0a8717a0c1b39
ats1999 <rkt82224@gmail.com>
2021-12-03 21:29:39 +0530

    fixed date

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,20 @@ import { useState } from "react";
 import Hero from "components/layout/Hero";
 import Aday from "@components/chart/Aday";
 import { Box, Input } from "@chakra-ui/react";
+
+import { to_YY_MM_DD } from "../lib/util";
+
 export default function Home() {
   let curDate = new Date();
-  const [date, setDate] = useState(
-    `${curDate.getFullYear()}-${curDate.getMonth()+1}-${curDate.getDate()}`
-  );
-
+  const [date, setDate] = useState(to_YY_MM_DD(new Date()));
   return (
     <div>
       <Box p={1}>
         <Input
           type="date"
-          value={date}
+          value={"2021-12-03"}
           onChange={(e) => setDate(e.target.value)}
-          // max={`${curDate.getFullYear()}-${curDate.getMonth()+1}-${curDate.getDate()}`}
+          max={to_YY_MM_DD(new Date())}
           w="200px"
         />
       </Box>
